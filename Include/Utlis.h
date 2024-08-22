3b15a39b98b7f3752c6400491d874ef4085872be
AmmarOsamaMalek <161338207+AmmarOsamaMalek@users.noreply.github.com>
2024-08-23 02:27:13 +0300

    Create Utlis.h

---
+++ Include/Utlis.h
@@ -0,0 +1,19 @@
+/*
+ * Utlis.h
+ *
+ *  Author: Ammar
+ */ 
+
+
+#ifndef UTLIS_H_
+#define UTLIS_H_
+
+
+#define WRITE_BIT(x,y,z)        x = ((x & ~(1<<y)) | (z<<y))
+#define READ_BIT(reg,bit)       ((reg>>bit)&1)
+#define SET_BIT(reg,bit)        reg = (reg|(1<<bit))
+#define CLEAR_BIT(reg,bit)      reg = (reg&~(1<<bit))
+#define TOGGLE_BIT(reg,bit)     reg = (reg^(1<<bit))
+
+
+#endif /* UTLIS_H_ */
